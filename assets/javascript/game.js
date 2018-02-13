fab2b47fe0f24f616b16d3f1784128405deff3b1
Morinventiv <brandonmorin@gmail.com>
2018-02-12 18:07:58 -0700

    tweeked scoring so it would reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,81 +1,87 @@
 // document.ready so everything loads at the same time
-$(document).ready(function() {
+$(document).ready(function () {
 
-// global variables
+	// global variables
 
-var targetNumber = 0;
-var currentScore = 0;
-var wins = 0;
-var losses = 0;
-var crystalVals = [];
+	var targetNumber = 0;
+	var currentScore = 0;
+	var wins = 0;
+	var losses = 0;
+	var crystalVals = [];
 
-// pick a random target number to aim for
-function targetNum() {
-	targetNumber = (Math.floor(Math.random() * 100) + 19);
-	$('#target-number').html(targetNumber);
-}
-
-// resets game
-function resetGame() {
-	targetNum();
-	currentScore = 0;
-	crystalVals();
-}
-
-// shows current score
-$('#player-score').html(currentScore);
-
-// random crystal values between 1 - 12
-function crysVal() {
-	for (let i = 0; i < 4; i++) {
-		var crysVal = (Math.floor(Math.random() * 11) + 1);
-		crystalVals.push(crysVal);
-		console.log(crystalVals);
+	// pick a random target number to aim for
+	function targetNum() {
+		targetNumber = (Math.floor(Math.random() * 100) + 19);
+		$('#target-number').html(targetNumber);
 	}
-}
 
-// runs functions
-targetNum();
-crysVal();
+	// resets game
+	function resetGame() {
+		targetNum();
+		currentScore = 0;
+		crysVal();
+		$('#player-score').html(currentScore);
+	}
 
-// on clicks
-$('#crystal1').on('click', function () {
-	currentScore = currentScore + crystalVals[0];
+	// shows current score
 	$('#player-score').html(currentScore);
-});
 
-$('#crystal2').on('click', function () {
-	currentScore = currentScore + crystalVals[1];
-	$('#player-score').html(currentScore);
-});
+	// random crystal values between 1 - 12
+	function crysVal() {
+		crystalVals = [];
+		for (let i = 0; i < 4; i++) {
+			var crysVal = (Math.floor(Math.random() * 11) + 1);
+			crystalVals.push(crysVal);
+			console.log(crystalVals);
+		}
+	}
 
-$('#crystal3').on('click', function () {
-	currentScore = currentScore + crystalVals[2];
-	$('#player-score').html(currentScore);
-});
+	// runs functions
+	targetNum();
+	crysVal();
 
-$('#crystal4').on('click', function () {
-	currentScore = currentScore + crystalVals[3];
-	$('#player-score').html(currentScore);
-});
+	// on clicks
+	$('#crystal1').on('click', function () {
+		currentScore = currentScore + crystalVals[0];
+		$('#player-score').html(currentScore);
+		comparison();
+	});
 
-// compare current score to target score
+	$('#crystal2').on('click', function () {
+		currentScore = currentScore + crystalVals[1];
+		$('#player-score').html(currentScore);
+		comparison();
+	});
 
-if (currentScore === targetNumber) {
-	wins++;
-	$('#winPanel').html(wins);
-	$('#winLosePanel').html("You Win!!");
-	resetGame();
-}
-else if (currentScore >= targetNumber) {
-	losses++;
-	$('#losePanel').html(losses);
-	$('#winLosePanel').html("You Lose!!");
-	resetGame();
-}
-else {
-	$('#winLosePanel').html("");
-}
+	$('#crystal3').on('click', function () {
+		currentScore = currentScore + crystalVals[2];
+		$('#player-score').html(currentScore);
+		comparison();
+	});
 
-// document.ready close
+	$('#crystal4').on('click', function () {
+		currentScore = currentScore + crystalVals[3];
+		$('#player-score').html(currentScore);
+		comparison();
+	});
+
+	// compare current score to target score
+	function comparison() {
+		if (currentScore === targetNumber) {
+			wins++;
+			$('#winPanel').html(wins);
+			$('#winLosePanel').html("You Win!!");
+			resetGame();
+		}
+		else if (currentScore >= targetNumber) {
+			losses++;
+			$('#losePanel').html(losses);
+			$('#winLosePanel').html("You Lose!!");
+			resetGame();
+		}
+		else {
+			$('#winLosePanel').html("");
+		}
+	}
+	// document.ready close
 });
